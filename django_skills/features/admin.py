738af74a846b83f6cd7f8bc05b3b9cafdabf5255
jagadeesh <djagadeeswar2001@gmail.com>
2021-07-03 14:17:33 +0530

    version 1.0

---
+++ django_skills/features/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from features.models import Info
+from features import views
+
+
+# Register your models here.
+admin.site.register(Info)
---
+++ django_skills/features/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from features import views
+urlpatterns = [
+    path('$/',views.index,name = 'info'),
+]
